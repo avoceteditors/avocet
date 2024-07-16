d2f55b6dd65729fcbbfd8dcc1131eacc1533cf42
Kenneth P. J. Dyer <kenneth@avoceteditors.com>
2024-07-16 15:09:28 -0500

    Word fix

--- eval/word.go
+++ eval/word.go
@@ -14,7 +14,7 @@ func (e *Evaluator) wordWorker(in <-chan *Line, wg sync.WaitGroup) {
 
 		ls := []string{}
 		for _, c := range l.Text {
-			if unicode.IsLetter(c) {
+			if unicode.IsLetter(c) || unicode.IsNumber(c) {
 				rs = append(rs, c)
 			} else if len(rs) > 0 {
 				ls = append(ls, string(rs))
