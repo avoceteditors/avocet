154599ca53bac36611dd8ba0f674d17a71ef0ba2
Kenneth P. J. Dyer <kenneth@avoceteditors.com>
2024-08-20 17:52:36 -0500

    4 Documents fs package (#21)
    * 4 Documents fs package
    
    * Updates text
    
    * Fixes go.mod

--- fs/read.go
+++ fs/read.go
@@ -4,10 +4,14 @@ import (
 	"os"
 )
 
-func ReadFile(f string) chan rune {
+// ReadFile takes a string filename and returns a rune channel.
+// Internally, it starts a goroutine that passes the file
+// content as runes to the channel.
+// If the file cannot be read, it returns a closed channel.
+func ReadFile(file string) chan rune {
 	con, err := os.ReadFile(f)
 	if err != nil {
-		lgr.Errorf("Read Error: %s\n%s", f, err)
+		lgr.Errorf("Read Error: %s\n%s", file, err)
 		ch := make(chan rune, 1)
 		close(ch)
 		return ch
--- fs/string.go
+++ fs/string.go
@@ -1,5 +1,8 @@
 package fs
 
-func ReadString(s string) chan rune {
-	return runeHandler([]rune(s))
+// ReadString takes a string and retunrs a rune channel. Internally,
+// it also starts a goroutine that passes the runes of the string
+// into the channel.
+func ReadString(content string) chan rune {
+	return runeHandler([]rune(content))
 }
