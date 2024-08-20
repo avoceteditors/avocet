51effbfa2e71a62d02d0ff157d431353e1b00b22
Kenneth P. J. Dyer <kenneth@avoceteditors.com>
2024-08-20 17:49:09 -0500

    Updates text

--- fs/read.go
+++ fs/read.go
@@ -7,10 +7,11 @@ import (
 // ReadFile takes a string filename and returns a rune channel.
 // Internally, it starts a goroutine that passes the file
 // content as runes to the channel.
-func ReadFile(f string) chan rune {
+// If the file cannot be read, it returns a closed channel.
+func ReadFile(file string) chan rune {
 	con, err := os.ReadFile(f)
 	if err != nil {
-		lgr.Errorf("Read Error: %s\n%s", f, err)
+		lgr.Errorf("Read Error: %s\n%s", file, err)
 		ch := make(chan rune, 1)
 		close(ch)
 		return ch
--- fs/string.go
+++ fs/string.go
@@ -3,6 +3,6 @@ package fs
 // ReadString takes a string and retunrs a rune channel. Internally,
 // it also starts a goroutine that passes the runes of the string
 // into the channel.
-func ReadString(s string) chan rune {
-	return runeHandler([]rune(s))
+func ReadString(content string) chan rune {
+	return runeHandler([]rune(content))
 }
