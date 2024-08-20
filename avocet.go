efcf3ae09e182e56c83913ba3d557e2ec4eabf32
Kenneth P. J. Dyer <kenneth@avoceteditors.com>
2024-08-20 17:53:16 -0500

    Bumps version

--- avocet.go
+++ avocet.go
@@ -5,4 +5,4 @@ package avocet
 // Version provides the current release of the Avocet Tools
 // package. It is primarily called by command-line applications
 // to report the version to stdout.
-const Version = "0.1.2"
+const Version = "0.1.3"
