224b46e86f81aeba8bc08912712b69b0ba08e9d0
Kenneth P. J. Dyer <kenneth@avoceteditors.com>
2024-08-21 18:01:50 -0500

    Implements token analysis functions and tests

---
+++ rst/token/analyze.go
@@ -0,0 +1,42 @@
+package token
+
+import "unicode"
+
+func (tok *Token) Analyze() {
+	switch tok.Rune {
+	case '*':
+		tok.Type = Star
+	case '`':
+		tok.Type = Tick
+	case ':':
+		tok.Type = Colon
+	case ' ':
+		tok.Type = Space
+	case '\t':
+		tok.Type = Space
+		tok.Append(' ')
+		tok.Append(' ')
+	case '\n':
+		tok.Type = EOL
+	case '.':
+		tok.Type = Dot
+	case '[':
+		tok.Type = OBrack
+	case ']':
+		tok.Type = CBrack
+	case '<':
+		tok.Type = OABrack
+	case '>':
+		tok.Type = CABrack
+	case '~':
+		tok.Type = Tilda
+	case '_':
+		tok.Type = Score
+	case '+':
+		tok.Type = Plus
+	default:
+		if unicode.IsNumber(tok.Rune) {
+			tok.Type = Num
+		}
+	}
+}
